46b9c6c91e3e7cd5e0ed9e0b3be9a8f0daa49074
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-11-06 15:53:33 +0100

    second ex. done

--- pexels.js
+++ pexels.js
@@ -38,3 +38,40 @@ const getPics = function () {
 }
 
 getPics()
+
+let secondaryButton = document.getElementById('secondaryButton')
+
+const getSecondaryPics = function () {
+  fetch("https://api.pexels.com/v1/search?query=storm", {
+    headers: {
+      Authorization: "MfgNTNXbLTeHd5I0hYZ6ysuW9g9oTl5TNbmhKJCGgMdI189h7xTzIz0D",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error(`${res.status}`)
+      }
+    })
+
+    .then((images) => {
+      const photosArray = { ...images.photos }
+      console.log(photosArray)
+      let imgsArray = document.getElementsByTagName("img")
+      secondaryButton.addEventListener(
+        "click",
+        (changeImg = () => {
+          for (let i = 0; i < imgsArray.length; i++) {
+            imgsArray[i].src = photosArray[i].src.medium
+          }
+        })
+      )
+    })
+
+    .catch((err) => {
+      console.log("ERRORE!", err)
+    })
+}
+
+getSecondaryPics()
